52d8a0066fbf488e26e9a33ad100d791559bda70
dmunozr <daniel.munoz.rivas@gmail.com>
2018-06-29 18:49:42 +0200

    Removing unused code.

--- consumer/src/main/java/com/demo/camel/converter/JSONToCarBudgetRequestTypeConverter.java
+++ consumer/src/main/java/com/demo/camel/converter/JSONToCarBudgetRequestTypeConverter.java
@@ -7,11 +7,8 @@ import java.io.InputStream;
 import java.io.ObjectOutputStream;
 import java.util.LinkedHashMap;
 
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
 
-import com.demo.dto.CarBudgetRequest;
 import com.google.common.base.Charsets;
 
 import net.minidev.json.JSONObject;
@@ -19,18 +16,12 @@ import net.minidev.json.JSONObject;
 import org.apache.camel.Converter;
 import org.apache.camel.TypeConverters;
 import org.apache.commons.io.IOUtils;
-import org.eclipse.persistence.jaxb.UnmarshallerProperties;
 
 @Converter
 public class JSONToCarBudgetRequestTypeConverter implements TypeConverters {
 
     @Converter
     public static java.io.InputStream toCarBudgetRequest(final LinkedHashMap linkedHashMap) throws JAXBException, IOException {
-
-        final JAXBContext jc = JAXBContext.newInstance(CarBudgetRequest.class);
-        final Unmarshaller unmarshaller = jc.createUnmarshaller();
-        unmarshaller.setProperty(UnmarshallerProperties.MEDIA_TYPE, "application/json");
-        unmarshaller.setProperty(UnmarshallerProperties.JSON_INCLUDE_ROOT, false);
         final JSONObject json = new JSONObject();
         json.putAll(linkedHashMap);
 
@@ -40,9 +31,7 @@ public class JSONToCarBudgetRequestTypeConverter implements TypeConverters {
     private static InputStream toInputStream(final Object obj) throws IOException {
         final ByteArrayOutputStream baos = new ByteArrayOutputStream();
         final ObjectOutputStream oos = new ObjectOutputStream(baos);
-
         oos.writeObject(obj);
-
         oos.flush();
         oos.close();
 
--- final-consumer/src/main/java/com/demo/camel/component/ImgFinderComponent.java
+++ final-consumer/src/main/java/com/demo/camel/component/ImgFinderComponent.java
@@ -1,5 +1,7 @@
 package com.demo.camel.component;
 
+import static com.demo.camel.component.ImgFinderConstants.SEARCH_ACTION_NAME;
+
 import java.util.Map;
 
 import org.apache.camel.Endpoint;
@@ -8,9 +10,15 @@ import org.apache.camel.impl.DefaultComponent;
 public class ImgFinderComponent extends DefaultComponent {
     @Override
     protected Endpoint createEndpoint(final String uri, final String remaining, final Map<String, Object> parameters) throws Exception {
-        final Endpoint endpoint = new ImgFinderEndpoint(uri, this);
-        setProperties(endpoint, parameters);
-        return endpoint;
+        if (SEARCH_ACTION_NAME.equals(remaining)) {
+            final Endpoint endpoint = new ImgFinderEndpoint(uri, this);
+            final ImgFinderConfiguration configuration = new ImgFinderConfiguration();
+            setProperties(configuration, parameters);
+            ((ImgFinderEndpoint) endpoint).setConfiguration(configuration);
+            return endpoint;
+        } else {
+            throw new IllegalArgumentException("Non-allowed operation path value: " + remaining);
+        }
     }
 
 }
